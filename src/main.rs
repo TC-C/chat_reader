08b94eec56875ed0499f7c9f85e86e378c7e923d
Pravin Ramana <pravinramana25@protonmail.ch>
2021-07-07 16:24:39 -0500

    User vod input setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io::{stdin, stdout, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut vod_link = String::new();
+    println!("This program supports Twitch VOD links");
+    print!("Enter link to VOD >>> ");
+    stdout()
+        .flush()
+        .expect("Could not flush line when preparing for <vod_link>");
+    stdin()
+        .read_line(&mut vod_link)
+        .expect("Could not read response <vod_link>");
+    vod_link = String::from(vod_link.trim_end_matches(&['\r', '\n'][..]));
 }
